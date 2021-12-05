281e64989cc37308188a8621682fd388ae6d5c8c
Pavel <plat956@gmail.com>
2021-12-05 12:15:32 +0300

    1

--- src/main/java/by/latushko/training/entity/Customer.java
+++ src/main/java/by/latushko/training/entity/Customer.java
@@ -31,31 +31,4 @@ public class Customer extends Thread{
     public void setHasPreorder(boolean hasPreorder) {
         this.hasPreorder = hasPreorder;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Customer customer = (Customer) o;
-
-        if (hasPreorder != customer.hasPreorder) return false;
-        return customerName != null ? customerName.equals(customer.customerName) : customer.customerName == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = customerName != null ? customerName.hashCode() : 0;
-        result = 31 * result + (hasPreorder ? 1 : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Customer{");
-        sb.append("customerName='").append(customerName).append('\'');
-        sb.append(", hasPreorder=").append(hasPreorder);
-        sb.append('}');
-        return sb.toString();
-    }
 }
